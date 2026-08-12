a4a9d496eac83da1c356f70f611b4f96a4c67550
Abhiraksha <abhiraksha.12@gmail.com>
2026-08-12 22:51:14 +0530

    Create string_practice.py

---
+++ Day04_Strings/string_practice.py
@@ -0,0 +1,12 @@
+word = input("Enter a word: ")
+
+print("Length:", len(word))
+
+print("First character:", word[0])
+print("Last character:", word[-1])
+
+print("First three characters:", word[:3])
+print("Reversed:", word[::-1])
+
+print("Uppercase:", word.upper())
+print("Lowercase:", word.lower())
