99145b82e0c1cb2cf753fb9706d7c3c116752df1
Abhiraksha <abhiraksha.12@gmail.com>
2026-08-12 22:40:07 +0530

    Create temperature_converter.py

---
+++ Day01_Basics/temperature_converter.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print("Hello", name)
+print("Temperature in Fahrenheit:", fahrenheit)
